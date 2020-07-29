1c7d6d558eed2da4339d0bb404e8782b177a441b
Jagger <jagarenkhzaya@gmail.com>
2020-07-29 11:14:29 +0200

    Map token stuff

--- static/javascript/map/setup.js
+++ static/javascript/map/setup.js
@@ -1,6 +1,6 @@
 function setup(mapid,mapcoords,mapzoom) {
   var mymap = L.map(mapid).setView(mapcoords, mapzoom);
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFnZ2VyMjg1IiwiYSI6ImNrZDc1Zzg2ajF5dmMydW50dms1Nzh4eXUifQ.GHuD7IxoAs1O0HxZ11OAVA', {
     maxZoom: 19,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
